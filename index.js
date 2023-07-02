a6bdfcca24af26a161ca9293eddc017abcb68b87
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-07-02 11:20:13 +0530

    Blog Project

---
+++ index.js
@@ -0,0 +1,12 @@
+const menuIcon=document.getElementById("menu_icon2");
+const menu=document.getElementById("menu-icon");
+
+menuIcon.addEventListener("click",()=>{
+    if(menu.className==="hidden"){
+        menu.classList.remove("hidden");
+    }
+    else{
+        menu.classList.add("hidden");
+
+    }
+})
